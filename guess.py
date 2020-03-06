38b08fcbccbbb00a96f2bcd609a95282d2e9dfdc
FLASElxx <61861339+FLASElxx@users.noreply.github.com>
2020-03-06 15:04:54 +0800

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,148 @@
+a=int(input('please enter a number'))
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+print('wait')
+b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+if a==b:
+	print('you are right')
+elif a>b:
+	print('too low')
+	b=int(input('please guess a number'))
+elif a<b:
+	print('too high')
+	b=int(input('please guess a number'))
+
